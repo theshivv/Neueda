6031ce7d336750f608f4ab4e30d2381b39a9ec57
theshivv <shivamdarasingpawar@gmail.com>
2022-07-07 10:28:43 +0530

    Footer edit

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -12,7 +12,7 @@ class FooterComponent extends Component {
         return (
             <div>
                 <footer className="footer">
-                    <span className="text-muted">All Rights Reserved 2022 @Team19 Neueda Training</span>
+                    <span className="text-muted">All Rights Reserved 2022 @Team26 Neueda Training</span>
                 </footer>
             </div>
         );
